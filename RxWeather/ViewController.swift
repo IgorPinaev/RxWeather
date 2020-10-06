9f1e833a9fafbfaf5c2e1aeb8ff6d3111c8a2060
igorpinaev <pinaevigor01@gmail.com>
2020-10-06 21:36:59 +0500

    Initial Commit

---
+++ RxWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxWeather
+//
+//  Created by Игорь Пинаев on 06.10.2020.
+//  Copyright © 2020 Igor Pinaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
